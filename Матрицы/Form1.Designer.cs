c0290492b88f0169dee170999cb818cf7a2040f4
Nikita Davydov <nikita.davidov02@yandex.ru>
2022-12-11 20:29:28 +0300

    Initial commit

---
+++ Матрицы/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace Матрицы
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.orderTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.CountDeterminant = new System.Windows.Forms.Button();
+            this.DeterminantIs = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // orderTextBox
+            // 
+            this.orderTextBox.Location = new System.Drawing.Point(161, 35);
+            this.orderTextBox.Name = "orderTextBox";
+            this.orderTextBox.Size = new System.Drawing.Size(75, 20);
+            this.orderTextBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Порядок матрицы";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(242, 33);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Построить матрицу";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // CountDeterminant
+            // 
+            this.CountDeterminant.Location = new System.Drawing.Point(381, 32);
+            this.CountDeterminant.Name = "CountDeterminant";
+            this.CountDeterminant.Size = new System.Drawing.Size(160, 23);
+            this.CountDeterminant.TabIndex = 3;
+            this.CountDeterminant.Text = "Вычислить определитель";
+            this.CountDeterminant.UseVisualStyleBackColor = true;
+            this.CountDeterminant.Click += new System.EventHandler(this.CountDeterminant_Click);
+            // 
+            // DeterminantIs
+            // 
+            this.DeterminantIs.AutoSize = true;
+            this.DeterminantIs.Location = new System.Drawing.Point(547, 38);
+            this.DeterminantIs.Name = "DeterminantIs";
+            this.DeterminantIs.Size = new System.Drawing.Size(47, 13);
+            this.DeterminantIs.TabIndex = 4;
+            this.DeterminantIs.Text = "det(A) = ";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(381, 61);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(160, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Обратная матрица";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1200, 800);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.DeterminantIs);
+            this.Controls.Add(this.CountDeterminant);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.orderTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox orderTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button CountDeterminant;
+        private System.Windows.Forms.Label DeterminantIs;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Матрицы/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Матрицы
+{
+    public partial class Form1 : Form
+    {
+        private int order;
+        private List<List<TextBox>> matrixOfTextBoxes = new List<List<TextBox>>();
+        private List<List<TextBox>> matrixOfLabels = new List<List<TextBox>>();
+        private List<List<float>> matrix = new List<List<float>>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Изменился порядок определителя
+            if (String.IsNullOrEmpty(orderTextBox.Text))
+                return;
+            order = Convert.ToInt32(orderTextBox.Text);
+            foreach (List<TextBox> listOfTextBoxes in matrixOfTextBoxes)
+            {
+                foreach (TextBox textBox in listOfTextBoxes)
+                {
+                    this.Controls.Remove(textBox);
+                }
+                //PerformLayout();
+            }
+            matrixOfTextBoxes.Clear();
+            matrix.Clear();
+            for (int i = 0; i < order; i++)
+            {
+                List<TextBox> listOfTextboxes = new List<TextBox>();
+                List<float> listOfFloats = new List<float>();
+                for (int j = 0; j < order; j++)
+                {
+                    TextBox textBox = new TextBox();
+                    textBox.Location = new Point(33 + (j * 85), 100 + (i * 30));
+                    textBox.Name = i.ToString() + j.ToString();
+                    textBox.Size = new Size(75, 40);
+                    textBox.TabIndex = 0;
+                    textBox.Text = "0";
+                    Controls.Add(textBox);
+                    listOfTextboxes.Add(textBox);
+
+                    float a;
+                    float.TryParse(textBox.Text, out a);
+                    listOfFloats.Add(a);
+                }
+                matrixOfTextBoxes.Add(listOfTextboxes);
+                matrix.Add(listOfFloats);
+
+            }
+        }
+        private void OutputMatrix(List<List<float>> matrix)
+        {
+            order = matrix.Count;
+            foreach (List<TextBox> listOfLabels in matrixOfLabels)
+            {
+                foreach (TextBox label in listOfLabels)
+                {
+                    this.Controls.Remove(label);
+                }
+                //PerformLayout();
+            }
+            matrixOfLabels.Clear();
+            for (int i = 0; i < order; i++)
+            {
+                List<TextBox> listOfLabels = new List<TextBox>();
+                //List<float> listOfFloats = new List<float>();
+                for (int j = 0; j < order; j++)
+                {
+                    TextBox textBox = new TextBox();
+                    textBox.Location = new Point(450 + (j * 85), 400 + (i * 30));
+                    textBox.Name = i.ToString() + j.ToString();
+                    textBox.Size = new Size(75, 40);
+                    textBox.TabIndex = 0;
+                    textBox.Text = matrix[i][j].ToString();
+                    Controls.Add(textBox);
+                    listOfLabels.Add(textBox);
+                }
+                matrixOfLabels.Add(listOfLabels);
+            }
+        }
+        private void UpdateMatrix()
+        {
+            for (int i = 0; i < order; i++)
+                for (int j = 0; j < order; j++)
+                {
+                    float a;
+                    float.TryParse(matrixOfTextBoxes[i][j].Text, out a);
+                    matrix[i][j] = a;
+                }
+        }
+        private void CountDeterminant_Click(object sender, EventArgs e)
+        {
+            UpdateMatrix();
+            DeterminantIs.Text = "det(A) = " + ReturnDeterminant(matrix).ToString();
+        }
+        private float ReturnDeterminant(List<List<float>> matrix)
+        {
+            int matrixOder = matrix.Count;
+            //Раскладываем по строке:
+            float determinant = 0;
+            for(int s = 0; s < matrixOder; s++)
+            {
+                //List<List<float>> minor = new List<List<float>>();
+                //for (int i = 1; i < matrixOder; i++)
+                //{
+                //    List<float> listOfFloats = new List<float>();
+                //    for (int j = 0; j < matrixOder; j++)
+                //        if (j != s)
+                //            listOfFloats.Add(matrix[i][j]);
+                //    minor.Add(listOfFloats);
+                //}
+                //float algebricAddition;
+                //if (minor.Count != 0)
+                //    algebricAddition = (float)Math.Pow(-1, s) * ReturnDeterminant(minor);
+                //else
+                //    algebricAddition = 1;
+                float algebricAddition = AlgebricAddition(matrix, 1, s+1);
+                determinant += matrix[0][s] * algebricAddition;
+            }
+            return determinant;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Обратная матрица
+            UpdateMatrix();
+            List<List<float>> inversedMatrix = InverseMatrix(matrix);
+            OutputMatrix(inversedMatrix);
+        }
+
+        private List<List<float>> InverseMatrix (List<List<float>> matrix)
+        {
+            List<List<float>> inversedMatrix = new List<List<float>>();
+            float matrixOder = matrix.Count;
+            for (int i = 0; i < matrixOder; i++)
+            {
+                List<float> listOfFloats = new List<float>();
+                for (int j = 0; j < matrixOder; j++)
+                {
+                    listOfFloats.Add(0);
+                }
+                inversedMatrix.Add(listOfFloats);
+            }
+            
+            float determinant = ReturnDeterminant(matrix);
+            if (determinant == 0)
+                return inversedMatrix;
+            for (int i = 1; i <= matrixOder; i++)
+                for(int j = 1; j <= matrixOder; j++)
+                {
+                    float algebricAddition = AlgebricAddition(matrix, j, i);
+                    inversedMatrix[i - 1][j - 1] = algebricAddition / determinant;
+                }
+            return inversedMatrix;
+        }
+        private float AlgebricAddition(List<List<float>> matrix, float rowFromOne, float columnFromOne)
+        {
+            float matrixOder = matrix.Count;
+            List<List<float>> minor = new List<List<float>>();
+            for (int i = 0; i < matrixOder; i++)
+            {
+                List<float> listOfFloats = new List<float>();
+                bool addRowToMinor = false;
+                for (int j = 0; j < matrixOder; j++)
+                    if (j != columnFromOne - 1 && i != rowFromOne - 1)
+                    {
+                        listOfFloats.Add(matrix[i][j]);
+                        addRowToMinor = true;
+                    }
+                if(addRowToMinor)
+                    minor.Add(listOfFloats);
+            }
+            float algebricAddition;
+            if (minor.Count != 0)
+                algebricAddition = (float)Math.Pow(-1, rowFromOne + columnFromOne) * ReturnDeterminant(minor);
+            else
+                algebricAddition = 1;
+            return algebricAddition;
+        }
+    }
+}
